e233fe7108397ba9de68ed67889c7848478b34c9
69MASKMAN <66298168+69MASKMAN@users.noreply.github.com>
2020-06-24 21:34:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,17 @@ require('firebase/database');
 
 // TODO: Replace the following with your app's Firebase project configuration
 // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  };
 
-//Ask author for configuration file
 
  // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
